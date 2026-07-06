3ad09d0b4969ca6f793effea3fa11e7886a05099
S. Avram <s.avram0@icloud.com>
2026-07-06 15:14:11 +0200

    Add an @Inline HTML fragment with Open Graph tags to it.

--- src/main/java/de/serav/cv/assistant/CvAssistent.java
+++ src/main/java/de/serav/cv/assistant/CvAssistent.java
@@ -1,10 +1,13 @@
 package de.serav.cv.assistant;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Inline;
 import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.component.page.TargetElement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Push
+@Inline(value = "og-meta.html", target = TargetElement.HEAD)
 @SpringBootApplication
 public class CvAssistent implements AppShellConfigurator {
 
